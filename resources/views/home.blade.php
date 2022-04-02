4dcc60a0b3d7b2c270cae294838fe686c275645f
mexudi <soufianmexudi20@gmail.com>
2022-04-02 22:33:27 +0000

    FrontEnd set up

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-3 ">
+            <img src="https://scontent.frba4-1.fna.fbcdn.net/v/t39.30808-6/272753339_128721316328211_2189194559123193580_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeEUurq9_m3EDijI_bCuNvqVO2-2taHXENc7b7a1odcQ1-2qstAr3FqMoiDcxAbEUroYW2JZI05hkoL4a3oipHzc&_nc_ohc=GKFfW9668D0AX-a0A1A&_nc_ht=scontent.frba4-1.fna&oh=00_AT8fwdW31ga-xUCU81zgbOyNP1FtYR21ehxsAdOw6DVcXg&oe=624CC649" alt="" class="rounded-circle" style="height: 250px;">
+        </div>
+        <div class="col-9 pt-5 pl-5">
+            <div>
+                <h1>XPR Campus</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>27</strong> post</div>
+                <div class="pr-5"><strong>2973</strong> followers</div>
+                <div class="pr-5"><strong>21</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
